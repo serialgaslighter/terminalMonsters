62250ae845058a5877d24d60a769c83fd64295ee
Julian P <julian-peukert@web.de>
2024-07-25 16:04:00 +0200

    add class Move

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ class Pokemon {
     this.level = level;
   }
 }
+
+class Move {
+  constructor(name, type, style, power, priority = 0) {
+    this.name = name;
+    this.type = type;
+    this.style = style
+    this.power = power;
+    this.priority = priority;
+  }
+}
