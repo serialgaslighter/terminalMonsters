f06c3a79058f600e2825bd63f372867b3f2c6ff9
Julian P <julian-peukert@web.de>
2024-07-25 16:03:06 +0200

    add class Pokemon

---
+++ main.js
@@ -0,0 +1,9 @@
+class Pokemon {
+  constructor(name, type, stats, moves, level = 50) {
+    this.name = name;
+    this.type = type;
+    this.stats = stats;
+    this.moves = moves;
+    this.level = level;
+  }
+}
