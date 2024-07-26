86377b7b24c8c7332d9ccee478577a1b0b54d5ce
Julian P <julian-peukert@web.de>
2024-07-26 10:56:07 +0200

    add function randomTeamGenerator

--- main.js
+++ main.js
@@ -90,6 +90,29 @@ const random = Math.random() * 0.15 + 0.85;
 const damage = Math.floor((22 * 90 * (129 / 100)) / 50 + 2);
 // console.log(random);
 // console.log(Math.floor(damage * random));
-console.log(Math.round(damage * random));
+// console.log(Math.round(damage * random));
 // console.log(Pokemon.pokedex);
-console.log(Pokemon.getAllPokemon());
+// console.log(Pokemon.getAllPokemon());
+
+// LOGIC
+// Get a random Team of 2 Pokemon
+function randomTeamGenerator(pokedex) {
+  
+  const myTeam = [];
+  while (myTeam.length < 2) {
+    const randomIndex = Math.floor(Math.random() * pokedex.length);
+
+    if (!myTeam.includes(pokedex[randomIndex])) {
+      myTeam.push(pokedex[randomIndex])
+    }
+  }
+  return myTeam;
+}
+// console.log(randomTeamGenerator(Pokemon.pokedex));
+const myTeam = randomTeamGenerator(Pokemon.pokedex);
+
+// Extrahiere die Namen der Pokémon
+const pokemonNames = myTeam.map(pokemon => pokemon.name);
+
+// Zeige die Namen in der Konsole an
+console.log(`Your Team is: ${pokemonNames.join(", ")}`);
