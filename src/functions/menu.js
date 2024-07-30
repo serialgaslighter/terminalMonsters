19e6affee059d78b06fde81535118d39553376f8
Julian P <julian-peukert@web.de>
2024-07-30 16:50:24 +0200

    doc combined menu if statement

--- src/functions/menu.js
+++ src/functions/menu.js
@@ -11,22 +11,16 @@ export function menuSelection(team) {
       console.clear();
       const chooseAttack = readlineSync.question(`[1] ${team[0].moves[0].name}\n[2] ${team[0].moves[1].name}\n[3] ${team[0].moves[2].name}\n[4] ${team[0].moves[3].name}\n[0] Back `)
 
-      if (chooseAttack === "1") {
+      if (
+        chooseAttack === "1" ||
+        chooseAttack === "2" ||
+        chooseAttack === "3" ||
+        chooseAttack === "4"
+      ) {
         console.clear();
-        console.log(`${team[0].name} uses ${team[0].moves[0].name}.`); 
+        console.log(`${team[0].name} uses ${team[0].moves[Number(chooseAttack - 1)].name}.`); 
       }
-      if (chooseAttack === "2") {
-        console.clear();
-        console.log(`${team[0].name} uses ${team[0].moves[1].name}.`); 
-      }
-      if (chooseAttack === "3") {
-        console.clear();
-        console.log(`${team[0].name} uses ${team[0].moves[2].name}.`); 
-      }
-      if (chooseAttack === "4") {
-        console.clear();
-        console.log(`${team[0].name} uses ${team[0].moves[3].name}.`); 
-      }
+      
     }
 
     if (select === "2") {
